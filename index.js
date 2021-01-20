d99ee3aa80fae753c474b1f7e71a496140fc0a37
Evan Darwin <evan@relta.net>
2021-01-19 21:00:22 -0800

    Why not

---
+++ index.js
@@ -0,0 +1,6 @@
+const webhooks = [
+	"https://discord.com/api/webhooks/801314049059979274/6mFN8x7p5eOttNgtIpKnppr2X-qKoJIN-JFCX0scQPxN7GDjPDSGWLnRazpa010CzUn_"
+];
+
+// hi
+// https://discord.com/api/webhooks/801314560883687443/uQPQR7BiqrGlKgPBsWvJnnjITW3Gu0F3arKhPi5b0cg0S9nKWthgDvj-RwBEF0SsWJbi
